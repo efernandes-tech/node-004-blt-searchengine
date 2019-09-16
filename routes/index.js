5b67cb34bd715e20b36111c63deca6b6eda7a318
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-09-15 21:48:57 -0300

    parte 6: programando a busca

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { results: false });
+});
+
+/* GET search page. */
+router.get('/search', function (req, res, next) {
+  var searchParams = req.query.query.toUpperCase().split(' ');
+  var db = require('../db');
+  var Cliente = db.Mongoose.model('clientes', db.ClienteSchema, 'clientes');
+  Cliente.find({ tags: { $all: searchParams } }, function (e, docs) {
+      res.render('index', { results: true, search: req.query.query, list: docs });
+  });
 });
 
 module.exports = router;
